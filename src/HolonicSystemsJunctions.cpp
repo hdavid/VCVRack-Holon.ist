e3651374bf159dd2334b46457dc6d75edcdbdd5f
henri <mots@motscousus.com>
2020-01-26 19:02:40 +0100

    add flip flop mode to Junctions

--- src/HolonicSystemsJunctions.cpp
+++ src/HolonicSystemsJunctions.cpp
@@ -4,6 +4,7 @@
 struct HolonicSystemsJunctionsModule : Module {
 
 	enum ParamIds {
+		TRIG_MODE_PARAM,
 		NUM_PARAMS
 	};
 
@@ -32,12 +33,12 @@ struct HolonicSystemsJunctionsModule : Module {
 	};
 	
 	LooseSchmittTrigger triggers[2];
+	bool isA[2] =  {true, true};
 	
 
 	HolonicSystemsJunctionsModule() {
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
 	}
-
 
 	~HolonicSystemsJunctionsModule() {
 	}
@@ -45,17 +46,50 @@ struct HolonicSystemsJunctionsModule : Module {
 	void onReset() override {
 	}
 
-
 	void process(const ProcessArgs &args) override {
 		for (int i=0; i<2; i++){
-			bool x = triggers[i].process(inputs[INPUT_1_CV+i].value);
-			bool a = !triggers[i].isHigh();
-			x = a && x;
-			outputs[OUTPUT_1+i].value = (a) ? inputs[INPUT_1_A+i].value : inputs[INPUT_1_B+i].value;
-			lights[LED_1_A+i].setSmoothBrightness(a ? 1 : 0, APP->engine->getSampleTime());
-			lights[LED_1_B+i].setSmoothBrightness(a ? 0 : 1, APP->engine->getSampleTime());
+			bool trigger = triggers[i].process(inputs[INPUT_1_CV+i].value);
+			if (params[TRIG_MODE_PARAM].value == 0){
+				isA[i] = !triggers[i].isHigh();
+			} else {
+				if (trigger) {
+					isA[i] = !isA[i];
+				}
+			}
+			outputs[OUTPUT_1+i].value = isA[i] ? inputs[INPUT_1_A+i].value : inputs[INPUT_1_B+i].value;
+			lights[LED_1_A+i].setSmoothBrightness(isA[i] ? 1 : 0, APP->engine->getSampleTime());
+			lights[LED_1_B+i].setSmoothBrightness(isA[i] ? 0 : 1, APP->engine->getSampleTime());
 		}
 	}
+	
+};
+
+
+struct HolonicJunctionTrigGateLabel : Widget {
+	int fontSize;
+	HolonicSystemsJunctionsModule *module = nullptr;
+
+	
+	HolonicJunctionTrigGateLabel(int _fontSize,HolonicSystemsJunctionsModule *_module) {
+		fontSize = _fontSize;
+		box.size.y = BND_WIDGET_HEIGHT;
+		module = _module;
+	}
+
+	void draw(const DrawArgs &args) override {
+		nvgFillColor(args.vg, nvgRGB(0, 0, 0));
+		nvgFontSize(args.vg, fontSize);
+		if (module){
+			if (module->params[HolonicSystemsJunctionsModule::TRIG_MODE_PARAM].value==0){
+				nvgText(args.vg, box.pos.x, box.pos.y, "gate", NULL);
+			} else {
+				nvgText(args.vg, box.pos.x, box.pos.y, "trig", NULL);
+			}
+		} else {
+			nvgText(args.vg, box.pos.x, box.pos.y, "trig", NULL);
+		}
+	}
+	
 };
 
 
@@ -65,6 +99,12 @@ struct HolonicSystemsJunctionsWidget : ModuleWidget {
 	HolonicSystemsJunctionsWidget(HolonicSystemsJunctionsModule *module) {
 		setModule(module);
 		setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/HolonicSystems-Junctions.svg")));
+		
+		addParam(createParam<CKSS>(Vec(43, 350), module, HolonicSystemsJunctionsModule::TRIG_MODE_PARAM));
+		HolonicJunctionTrigGateLabel* const trigGateLabel = new HolonicJunctionTrigGateLabel(10, module);
+		trigGateLabel->box.pos = Vec(10, 355/2+5);
+		addChild(trigGateLabel);
+		
 		
 		//screws
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
